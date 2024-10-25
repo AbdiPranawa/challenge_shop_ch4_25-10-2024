1fa4a7df2cbedd985e9d475c38f281338afdbb4b
AbdiPranawa <asaturaardana@gmail.com>
2024-10-25 09:49:51 +0700

    add files

---
+++ controllers/userController.js
@@ -0,0 +1,112 @@
+const { Users } = require("../models");
+const { Op } = require("sequelize");
+
+const findUsers = async (req, res, next) => {
+  try {
+    const { userName, role, page = 1, limit = 10 } = req.query;
+
+    const condition = {};
+    if (userName) condition.name = { [Op.iLike]: `%${userName}%` };
+    if (role) condition.role = { [Op.iLike]: `%${role}%` };
+
+    const offset = (page - 1) * limit;
+
+    const users = await Users.findAll({
+      attributes: ["name", "role"],
+      where: condition,
+      limit: limit,
+      offset: offset,
+    });
+
+    const totalData = users.length;
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        totalData,
+        users,
+      },
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Failed",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    }
+  }
+};
+
+const findUserById = async (req, res, next) => {
+  try {
+    const user = await Users.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {}
+};
+
+const updateUser = async (req, res, next) => {
+  const { name, age, role, address, shopId } = req.body;
+  try {
+    await Users.update(
+      {
+        name,
+        age,
+        role,
+        address,
+        shopId,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    res.status(200).json({
+      status: "Success",
+      message: "sukses update user",
+    });
+  } catch (err) {}
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await Users.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    await Users.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({
+      status: "Success",
+      message: "sukses delete user",
+    });
+  } catch (err) {}
+};
+
+module.exports = {
+  findUsers,
+  findUserById,
+  updateUser,
+  deleteUser,
+};
---
+++ seeders/20241021000003-seed-users.js
@@ -0,0 +1,24 @@
+"use strict";
+const { faker } = require("@faker-js/faker");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = [];
+    for (let i = 0; i < 100; i++) {
+      users.push({
+        name: faker.person.fullName(),
+        age: faker.number.int({ min: 18, max: 65 }),
+        address: faker.location.streetAddress(),
+        role: faker.helpers.arrayElement(["user", "admin"]),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+    await queryInterface.bulkInsert("Users", users, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
